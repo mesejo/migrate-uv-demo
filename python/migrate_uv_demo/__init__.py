3ea1457f8d304e6694d72027126c23c7aa114ef2
Daniel Mesejo <mesejoleon@gmail.com>
2025-01-06 15:47:23 +0100

    chore: migrate pyproject.toml to uv

--- python/migrate_uv_demo/__init__.py
+++ python/migrate_uv_demo/__init__.py
@@ -1,6 +1,10 @@
+import importlib.metadata
+
 from .migrate_uv_demo import *
 
 
 __doc__ = migrate_uv_demo.__doc__
 if hasattr(migrate_uv_demo, "__all__"):
     __all__ = migrate_uv_demo.__all__
+
+__version__ = importlib.metadata.version(__package__)
